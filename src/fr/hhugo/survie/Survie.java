baa1a55f703691423dd1ec80451238f88e8aadf1
Hugo <hugo.horlacher@gmail.com>
2024-06-15 13:26:16 +0200

    Ajout messages actif/inactif

--- src/fr/hhugo/survie/Survie.java
+++ src/fr/hhugo/survie/Survie.java
@@ -1,26 +1,29 @@
 package fr.hhugo.survie;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Survie extends JavaPlugin
 {
-
-    private static final Survie INSTANCE = new Survie();
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     @Override
     public void onEnable()
     {
-        getLogger().info("Survie is enabled");
+        getLogger().info(ANSI_WHITE_BACKGROUND + ANSI_GREEN + "Le plugin de Survie est actif" + ANSI_RESET);
     }
 
     @Override
     public void onDisable()
     {
-        getLogger().info("Survie is disabled");
+        getLogger().info(ANSI_WHITE_BACKGROUND + ANSI_RED + "Le plugin de survie n'est plus actif" + ANSI_RESET);
     }
 
     public static Survie getInstance()
     {
-        return INSTANCE;
+        return getPlugin(Survie.class);
     }
 }
